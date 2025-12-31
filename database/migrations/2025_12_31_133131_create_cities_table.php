cb79751ef68d1474ba78fc08dd07810c345e9cea
Isann22 <rihsan900@Gmail.com>
2025-12-31 21:24:22 +0700

    add migration&model city, setup spatie media library

---
+++ database/migrations/2025_12_31_133131_create_cities_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name', 255)->nullable(value: false);
+            $table->string('slug')->unique()->nullable(value: true);
+            $table->text('details')->nullable(value: true);
+            $table->decimal('price', 15, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cities');
+    }
+};
