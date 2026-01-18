14a961d41f5737d6d4389a7e331afcd887084e20
Isann22 <rihsan900@Gmail.com>
2026-01-18 14:54:56 +0700

    fix package seeder

--- database/seeders/PackageSeeder.php
+++ database/seeders/PackageSeeder.php
@@ -13,36 +13,33 @@ class PackageSeeder extends Seeder
      */
     public function run(): void
     {
-        // Data Package
+        // Data Package (Konfigurasi durasi dan jumlah foto)
         $packagesData = [
             [
                 'name' => '1 Hour Photo Shoot',
                 'hour_duration' => 1,
                 'downloadable_photos' => 20,
                 'edited_photos' => 50,
+                'multiplier' => 1.0, // Harga dasar (100%)
             ],
             [
                 'name' => '2 Hours Photo Shoot',
                 'hour_duration' => 2,
                 'downloadable_photos' => 40,
                 'edited_photos' => 100,
+                'multiplier' => 1.8, // Diskon durasi (misal: lebih murah dibanding beli 1 jam x 2)
             ],
             [
                 'name' => '3 Hours Photo Shoot',
                 'hour_duration' => 3,
                 'downloadable_photos' => 60,
                 'edited_photos' => 150,
+                'multiplier' => 2.5, // Lebih hemat untuk durasi panjang
             ],
         ];
 
-        // Harga per Kota
-        $cityPricing = [
-            'Jakarta' => [169, 299, 399],
-            'Bandung' => [149, 269, 359],
-            'Yogyakarta' => [129, 229, 319],
-        ];
-
-        foreach ($packagesData as $index => $data) {
+        foreach ($packagesData as $data) {
+            // 1. Buat data Package
             $package = Package::create([
                 'name' => $data['name'],
                 'hour_duration' => $data['hour_duration'],
@@ -52,17 +49,20 @@ class PackageSeeder extends Seeder
 
             $this->command->info("Seeding package: {$package->name}...");
 
-            // Attach ke semua kota dengan harga berbeda
+            // 2. Ambil semua kota dari database
             $cities = City::all();
+
             foreach ($cities as $city) {
-                $prices = $cityPricing[$city->name] ?? [150, 250, 350];
-                $price = $prices[$index] ?? $prices[0];
+
+                $basePrice = $city->price;
+                $finalPrice = $basePrice * $data['multiplier'];
 
                 $city->packages()->attach($package->id, [
-                    'price' => $price,
+                    'price' => $finalPrice,
                 ]);
 
-                $this->command->info("   - {$city->name}: \${$price}");
+                $formattedPrice = number_format($finalPrice, 0, ',', '.');
+                $this->command->info("   - {$city->name}: Rp {$formattedPrice}");
             }
         }
     }
