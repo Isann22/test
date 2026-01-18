c36180dcba17bd9d2293c41c65d5b644bbedc3c1
Isann22 <rihsan900@Gmail.com>
2026-01-18 17:32:11 +0700

    add edit profile

--- resources/views/components/user-drawer.blade.php
+++ resources/views/components/user-drawer.blade.php
@@ -14,7 +14,7 @@
         <div x-show="open" x-transition:enter="transition ease-in-out duration-300 transform"
             x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0"
             x-transition:leave="transition ease-in-out duration-300 transform" x-transition:leave-start="translate-x-0"
-            x-transition:leave-end="translate-x-full" class="fixed top-0 right-0 h-full w-80 bg-base-200 z-[999] shadow-xl"
+            x-transition:leave-end="translate-x-full" class="fixed top-0 right-0 h-full w-60 bg-base-200 z-[999] shadow-xl"
             x-cloak>
 
             {{-- Close Button --}}
@@ -22,7 +22,7 @@
                 <x-mary-icon name="o-x-mark" class="w-5 h-5" />
             </button>
 
-            <div class="p-6 pt-4">
+            <div class="p-0 pt-4">
                 {{-- User Info --}}
                 <div class="flex flex-col items-center gap-4 mb-2">
                     <div class="text-center">
@@ -37,25 +37,31 @@
                 <ul class="menu gap-2">
                     @if (!auth()->user()->hasVerifiedEmail())
                         <li>
-                            <a href="{{ route('verification.notice') }}" wire:navigate class="btn btn-primary">
+                            <x-nav-link href="{{ route('verification.notice') }}" :active="request()->routeIs('verification.notice')" wire:navigate>
                                 <x-mary-icon name="o-envelope" class="w-5 h-5" />
                                 Verify Email
-                            </a>
+                            </x-nav-link>
+                        </li>
+                    @else
+                        <li>
+                            <x-mary-button label="Profile" icon="o-user" link="{{ route('settings') }}" class="w-full" />
+                        </li>
+
+                        <li>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                @method('DELETE')
+                                <x-mary-button label="Logout" icon="o-arrow-right-on-rectangle" class="w-full" />
+                            </form>
                         </li>
                     @endif
 
-                    <li>
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="w-full flex items-center gap-2">
-                                <x-mary-icon name="o-arrow-right-on-rectangle" class="w-5 h-5" />
-                                Log Out
-                            </button>
-                        </form>
-                    </li>
+
                 </ul>
+
             </div>
+
+
         </div>
     </div>
 @endauth
