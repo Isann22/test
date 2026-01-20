62d9dad3e294370127274c3ef5f1f2c4a1a4d505
Isann22 <rihsan900@Gmail.com>
2026-01-20 21:57:30 +0700

    resolve conflict js filament

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html data-theme="corporate" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html data-theme="lofi" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="utf-8">
