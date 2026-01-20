bd1b62e4187d4defe3727ddf8f04205f4f334644
Isann22 <rihsan900@Gmail.com>
2026-01-21 01:04:48 +0700

    add status column to applicants & enum status

---
+++ database/migrations/2026_01_20_180232_add_status_to_photogrpher_applicants_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('photographer_applicants', function (Blueprint $table) {
+            //
+            $table->string('status', 20)->default('review');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('photographer_applicants', function (Blueprint $table) {
+            //
+        });
+    }
+};
